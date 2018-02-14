296097b4da1e3ffdc7bd9a0e738782fffe036344
GR <gongran@foxmail.com>
2018-02-14 22:31:47 +0800

    666

--- src/main/java/com/gr/market/constant/ChainBlockConstant.java
+++ src/main/java/com/gr/market/constant/ChainBlockConstant.java
@@ -2,7 +2,7 @@ package com.gr.market.constant;
 
 public class ChainBlockConstant {
 	public final static String KEY="baa2ec0c-0cab5648-6f089f58-0433b";
-	public final static String SECRET="28af774e-88e01954-6ad3f167-6d7e5";
+	public final static String SECRET="6d7e5-28af774e-88e01954-6ad3f167";
 	public final static String[] MAINCOIN = { "", "", "" };
 
 }
